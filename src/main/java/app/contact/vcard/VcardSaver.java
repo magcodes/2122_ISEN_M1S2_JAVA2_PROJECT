7839f53bbdb9f97b3faf463ea07d883187194f60
unknown <williamjnrkdd@gmail.com>
2022-03-13 17:12:13 +0100

    vcard functionality building in progress

--- src/main/java/app/contact/vcard/VcardSaver.java
+++ src/main/java/app/contact/vcard/VcardSaver.java
@@ -6,20 +6,30 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import app.contact.entities.Person;
+import app.contact.views.App;
 
 public class VcardSaver {
 	
-//	public void save(Person person) {
-//		try {
-////			String messageToWrite = """
-////					ddsada
-////					dsadsd"""""";
-////			Files.writeString(Paths.get(person.getId()+".vcr"), messageToWrite, StandardCharsets.UTF_8);//
-//		}
-//		catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public void save(Person person) {
+		try {
+			String messageToWrite = """
+					BEGIN:VCARD
+					VERSION:4.0
+					N:%s
+					FN:%s
+					NICKNAME:%s
+					TEL;TYPE=cell:%s
+					ADR;TYPE=home:;;%s;;;;
+					EMAIL:%s
+					BDAY:%s
+					END:VCARD""".formatted(person.getLastName(), person.getFirstName(),person.getNickName(),
+							person.getPhoneNumber(), person.getAddress(), person.getEmailAddress(), person.getBirthDate().toString().replace("-", ""));
+			Files.writeString(Paths.get(person.getId()+".vcf"), messageToWrite, StandardCharsets.UTF_8);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
 
